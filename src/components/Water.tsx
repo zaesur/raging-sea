ad3b158f6cf03d77441bd8c788292cfceedbdd75
Florian Van Belleghem <fvbe@stylelabs.com>
2022-11-21 21:47:33 +0100

    Implement big waves

--- src/components/Water.tsx
+++ src/components/Water.tsx
@@ -1,15 +1,49 @@
-import { Euler } from "three";
-import { FunctionComponent } from "react";
+import { DoubleSide, Euler, ShaderMaterial } from "three";
+import { FunctionComponent, useRef } from "react";
 import vertexShader from "../shaders/water/vertex.glsl";
 import fragmentShader from "../shaders/water/fragment.glsl";
+import { useControls } from "leva";
+import { useFrame } from "@react-three/fiber";
 
 const Water: FunctionComponent = () => {
+  const shaderRef = useRef<ShaderMaterial>(null!);
+  const controls = useControls({
+    bigWavesElevation: {
+      value: 0.2,
+      min: 0,
+      max: 1,
+      step: 0.001,
+    },
+    bigWavesFrequency: {
+      value: [4, 1.5],
+    },
+    bigWavesSpeed: {
+      value: 0.75,
+      min: 0,
+      max: 4,
+    },
+  });
+
+  useFrame(({ clock }) => {
+    shaderRef.current.uniforms.uTime.value = clock.getElapsedTime();
+  });
+
   return (
     <mesh rotation={new Euler(-Math.PI * 0.5)}>
-      <planeBufferGeometry args={[2, 2, 128, 128]} />
+      <planeGeometry args={[2, 2, 128, 128]} />
       <shaderMaterial
+        side={DoubleSide}
+        key={controls.bigWavesElevation}
+        ref={shaderRef}
         vertexShader={vertexShader}
         fragmentShader={fragmentShader}
+        defines={{ PI: Math.PI }}
+        uniforms={{
+          uTime: { value: 0 },
+          uBigWavesElevation: { value: controls.bigWavesElevation },
+          uBigWavesFrequency: { value: controls.bigWavesFrequency },
+          uBigWavesSpeed: { value: controls.bigWavesSpeed },
+        }}
       />
     </mesh>
   );
--- src/shaders/water/vertex.glsl
+++ src/shaders/water/vertex.glsl
@@ -1,8 +1,24 @@
+uniform float uTime;
+uniform float uBigWavesElevation;
+uniform vec2 uBigWavesFrequency;
+uniform float uBigWavesSpeed;
+
 varying vec2 vUv;
 
 void main()
 {
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    // Elevation
+    float elevation =
+      sin(modelPosition.x * uBigWavesFrequency.x + uTime * uBigWavesSpeed) *
+      sin(modelPosition.z * uBigWavesFrequency.y + uTime * uBigWavesSpeed) *
+      uBigWavesElevation;
+    modelPosition.y += elevation;
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+
+    gl_Position = projectionMatrix * viewPosition;
 
     vUv = uv;
 }
