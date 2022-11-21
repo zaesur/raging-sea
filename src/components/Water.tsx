fc0ae33d6d8f99c546f743ad28be99b4474499ae
Florian Van Belleghem <fvbe@stylelabs.com>
2022-11-21 21:02:07 +0100

    Setup shader

--- src/components/Water.tsx
+++ src/components/Water.tsx
@@ -1,11 +1,16 @@
 import { Euler } from "three";
 import { FunctionComponent } from "react";
+import vertexShader from "../shaders/water/vertex.glsl";
+import fragmentShader from "../shaders/water/fragment.glsl";
 
 const Water: FunctionComponent = () => {
   return (
     <mesh rotation={new Euler(-Math.PI * 0.5)}>
       <planeBufferGeometry args={[2, 2, 128, 128]} />
-      <meshBasicMaterial />
+      <shaderMaterial
+        vertexShader={vertexShader}
+        fragmentShader={fragmentShader}
+      />
     </mesh>
   );
 };
